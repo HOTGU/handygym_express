cca51a32d3db15834c94a09dcefff47861efdc38
hangu <gksrn42@gmail.com>
2022-11-15 14:09:41 +0900

    set up

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("hello world !!");
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+
+const mongoUrl = process.env.DEV_MONGO_URL;
+const app = express();
+const port = process.env.PORT || 5000;
+
+mongoose.connect(mongoUrl);
+
+const db = mongoose.connection;
+
+const handleDBError = () => console.log("❌DB연결 실패");
+const handleDBSuccess = () => console.log(`✅DB연결 성공`);
+
+db.on("error", handleDBError);
+db.once("open", handleDBSuccess);
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const handleListen = () => console.log(`✅서버가 ${port}에서 실행중입니다`);
+
+app.listen(port, handleListen);
